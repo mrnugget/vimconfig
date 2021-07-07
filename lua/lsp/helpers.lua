3019dc2377892d9e1433e2d5adef102ba2491e42
Thorsten Ball <mrnugget@gmail.com>
2021-07-07 17:53:26 +0200

    Restore old goimports after fixing all the other stuff

--- lua/lsp/helpers.lua
+++ lua/lsp/helpers.lua
@@ -10,8 +10,22 @@ helpers.format_rust = function()
   vim.api.nvim_win_set_cursor(0, lineno)
 end
 
-helpers.goimports = function()
-    vim.lsp.buf.formatting_sync(nil, 1000)
+-- Taken from here: https://github.com/neovim/nvim-lspconfig/issues/115
+helpers.goimports = function(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+
+  vim.lsp.buf.formatting()
 end
 
 return helpers
