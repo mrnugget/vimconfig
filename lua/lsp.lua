ed4ef1e2922e0d936dae9438090a50465f1706cb
Thorsten Ball <mrnugget@gmail.com>
2022-02-09 09:16:37 +0100

    Update null-ls configuration

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -38,6 +38,8 @@ local on_attach = function(client, bufnr)
     -- Requirements:
     --   npm install -g typescript-language-server prettier eslint_d
     --   asdf reshim nodejs
+    --
+    -- See the null-ls setup below for prettier/eslint_d config
 
     -- disable tsserver formatting because we use prettier/eslint for that
     client.resolved_capabilities.document_formatting = false
@@ -49,16 +51,6 @@ local on_attach = function(client, bufnr)
 
     local ts_utils = require("nvim-lsp-ts-utils")
     ts_utils.setup {
-        -- eslint_enable_code_actions = true,
-        -- eslint_enable_disable_comments = true,
-        -- eslint_bin = "eslint_d",
-        -- eslint_enable_diagnostics = true,
-        -- eslint_show_rule_id = true,
-
-        -- enable_formatting = true,
-        -- formatter = "prettier",
-
-        -- inlay hints
         auto_inlay_hints = true,
         inlay_hints_highlight = "Whitespace",
         inlay_hints_priority = 200, -- priority of the hint extmarks
@@ -167,13 +159,13 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint_d.with({
-      filetypes = { "typescriptreact", "typescript", "javascript", "javascriptreact"},
+      filetypes = { "typescriptreact", "scss", "typescript", "javascript", "javascriptreact"},
     }),
     null_ls.builtins.code_actions.eslint_d.with({
-      filetypes = { "typescriptreact", "typescript", "javascript", "javascriptreact"},
+      filetypes = { "typescriptreact", "scss", "typescript", "javascript", "javascriptreact"},
     }),
     null_ls.builtins.formatting.prettier.with({
-      filetypes = { "typescriptreact", "typescript", "javascript", "javascriptreact"},
+      filetypes = { "typescriptreact", "scss", "typescript", "javascript", "javascriptreact"},
       prefer_local = "node_modules/.bin"
     }),
   }
