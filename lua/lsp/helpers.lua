a0719a335ac7397d900b5b1864cad1853ebde6b2
Thorsten Ball <mrnugget@gmail.com>
2022-02-11 10:52:05 +0100

    Fix LSP formatting with newest neovim version

--- lua/lsp/helpers.lua
+++ lua/lsp/helpers.lua
@@ -18,7 +18,7 @@ helpers.goimports = function(wait_ms)
   for _, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit)
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
       else
         vim.lsp.buf.execute_command(r.command)
       end
