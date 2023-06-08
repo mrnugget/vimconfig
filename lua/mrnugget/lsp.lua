2b69a07f7e121cdd318ba89d12498404ca3093ea
Thorsten Ball <mrnugget@gmail.com>
2023-06-08 12:21:15 +0200

    Fix semantic highlighting after upgrading to nvim 0.9.1

--- lua/mrnugget/lsp.lua
+++ lua/mrnugget/lsp.lua
@@ -444,6 +444,28 @@ vim.api.nvim_set_keymap(
 require("fidget").setup {}
 
 -------------------------------------------------------------------------------
+-- Semantic Highlighting
+-------------------------------------------------------------------------------
+local links = {
+  ["@lsp.type.namespace"] = "@namespace",
+  ["@lsp.type.type"] = "@type",
+  ["@lsp.type.class"] = "@type",
+  ["@lsp.type.enum"] = "@type",
+  ["@lsp.type.interface"] = "@type",
+  ["@lsp.type.struct"] = "@structure",
+  ["@lsp.type.parameter"] = "Normal",
+  ["@lsp.type.variable"] = "@variable",
+  ["@lsp.type.property"] = "@property",
+  ["@lsp.type.enumMember"] = "@constant",
+  ["@lsp.type.function"] = "@function",
+  ["@lsp.type.method"] = "@method",
+  ["@lsp.type.macro"] = "@macro",
+  ["@lsp.type.decorator"] = "@function",
+}
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
+-------------------------------------------------------------------------------
 -- Helpers
 -------------------------------------------------------------------------------
 
