ea9558ffc90925713284a92dff18796509120d6d
Thorsten Ball <mrnugget@gmail.com>
2023-09-06 08:55:03 +0200

    More Cody configuration

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -7,6 +7,13 @@ local lspkind = require "lspkind"
 local luasnip = require "luasnip"
 
 cmp.setup {
+  sources = {
+    { name = "nvim_lsp", keyword_length = 3 },
+    { name = "cody" },
+    { name = "luasnip", keyword_length = 3 },
+    { name = "buffer", keyword_length = 5 },
+    { name = "path", keyword_length = 3 },
+  },
   formatting = {
     format = lspkind.cmp_format {
       with_text = true,
@@ -34,7 +41,7 @@ cmp.setup {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    -- ["<CR>"] = cmp.mapping.confirm { select = true },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -45,14 +52,8 @@ cmp.setup {
       end
     end, { "i", "s" }),
   },
-  sources = {
-    { name = "nvim_lsp", keyword_length = 3 },
-    { name = "cody" },
-    { name = "luasnip", keyword_length = 3 },
-    { name = "buffer", keyword_length = 5 },
-    { name = "path", keyword_length = 3 },
-  },
 }
+
 cmp.setup.filetype("markdown", {
   sources = cmp.config.sources {
     { name = "path" }, -- You can specify the `cmp_git` source if you were installed it.
